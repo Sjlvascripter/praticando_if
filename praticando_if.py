c1f2f646811136a8858cb77bee048e9f09da5fe9
Sjlvascripter <silvascripter@gmail.com>
2024-08-12 19:26:57 -0300

    primeiro commit

---
+++ praticando_if.py
@@ -0,0 +1,9 @@
+IDADE_MINIMA = 18
+
+Idade = float(input("Informe sua idade"))
+
+if Idade >= IDADE_MINIMA:
+    print("Requisito de idade minima atendido")
+
+if Idade < IDADE_MINIMA:
+    print("Requisito de idade minima nao atendido")
